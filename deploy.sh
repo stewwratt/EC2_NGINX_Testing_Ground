2f0c4dcc43af16747e1421eaa54f0c9aa2dab4df
stewwratt <92232004+stewwratt@users.noreply.github.com>
2024-08-25 20:07:12 +1000

    fix: wrong file path

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #!/bin/bash
-cd /home/ubuntu/your_app
+cd /home/ubuntu/test_app
 git pull origin main  # Pull the latest code from GitHub
 sudo systemctl restart nginx  # Restart Nginx to apply any changes
 
 
 # adding comment to test post push hook
+
+
 
